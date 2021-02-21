a3a21dfb5861e4d81851e61bc22c0be0cc3d866b
shun <mvshun@gmail.com>
2021-02-21 16:14:00 +0800

    add a sub function

--- main.c
+++ main.c
@@ -6,8 +6,10 @@ int main()
     int a = 10;
     int b = 5;
     int c = Add(a, b);
+    int d = Sub(a, b);
 
     printf("%d + %d = %d\n", a, b, c);
+    printf("%d - %d = %d\n", a, b, d);
 
     return 0;
 }
