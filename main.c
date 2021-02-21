7e487f76595c9f1df85ca038a3d6dcabd020c004
shun <mvshun@gmail.com>
2021-02-21 21:00:49 +0800

    add second submodule

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <fun_add.h>
+#include <fun_mul.h>
 
 int main()
 {
@@ -7,9 +8,11 @@ int main()
     int b = 5;
     int c = Add(a, b);
     int d = Sub(a, b);
+    int e = Mul(a, b);
 
     printf("%d + %d = %d\n", a, b, c);
     printf("%d - %d = %d\n", a, b, d);
+    printf("%d * %d = %d\n", a, b, e);
 
     return 0;
 }
